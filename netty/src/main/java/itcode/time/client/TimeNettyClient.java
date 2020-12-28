19829678a4b46a88edab0ff7118d757cd0468a26
yangcheng <cheng.yang@mail.hypers.com>
2020-12-28 17:42:24 +0800

    导入netty

---
+++ netty/src/main/java/itcode/time/client/TimeNettyClient.java
@@ -0,0 +1,13 @@
+package itcode.time.client;
+
+/**
+ * @author imp
+ * @date 2020/12/28
+ */
+public class TimeNettyClient {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ netty/src/main/java/itcode/time/server/TimeNettyServer.java
@@ -0,0 +1,13 @@
+package itcode.time.server;
+
+/**
+ * @author imp
+ * @date 2020/12/28
+ */
+public class TimeNettyServer {
+
+    public static void main(String[] args) {
+
+    }
+
+}
