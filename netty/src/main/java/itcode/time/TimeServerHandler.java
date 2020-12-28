59d27215919ccb946b5070f016769e54f87fd8eb
yangcheng <cheng.yang@mail.hypers.com>
2020-12-28 18:05:25 +0800

    timeserver

---
+++ netty/src/main/java/itcode/time/TimeServerHandler.java
@@ -0,0 +1,9 @@
+package itcode.time;
+
+/**
+ * @author imp
+ * @date 2020/12/28
+ */
+public class TimeServerHandler {
+
+}
--- netty/src/main/java/itcode/time/server/TimeNettyServer.java
+++ netty/src/main/java/itcode/time/server/TimeNettyServer.java
@@ -1,13 +1,49 @@
 package itcode.time.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 /**
  * @author imp
  * @date 2020/12/28
  */
 public class TimeNettyServer {
 
-    public static void main(String[] args) {
+    public void bind(int port) {
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
 
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(boss, worker)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .childHandler(new ChildChannelHandler());
+            ChannelFuture f = bootstrap.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } catch (Exception e) {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 8080;
+        new TimeNettyServer().bind(port);
     }
 
 }
