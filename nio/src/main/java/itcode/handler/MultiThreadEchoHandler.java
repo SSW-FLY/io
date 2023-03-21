25dbb85f6be33e78d399d18a1e745b6c416d4d08
yangcheng <cheng.yang@mail.hypers.com>
2023-03-21 18:19:10 +0800

    多线程demo

---
+++ nio/src/main/java/itcode/handler/MultiThreadEchoHandler.java
@@ -0,0 +1,60 @@
+package itcode.handler;
+
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MultiThreadEchoHandler implements Runnable {
+    final SocketChannel channel;
+
+    final SelectionKey sk;
+
+    final ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+    static final int RECEIVING = 0, SENDING = 1;
+
+    int state = RECEIVING;
+
+    static ExecutorService pool = Executors.newFixedThreadPool(4);
+
+    public MultiThreadEchoHandler(Selector selector, SocketChannel c) throws Exception {
+        this.channel = c;
+        c.configureBlocking(false);
+        sk = channel.register(selector, 0);
+        sk.attach(this);
+        sk.interestOps(SelectionKey.OP_READ);
+        selector.wakeup();
+    }
+
+    @Override
+    public void run() {
+        pool.submit(() -> asyncRun());
+    }
+
+    public synchronized void asyncRun() {
+        try {
+            if (state == SENDING) {
+                channel.write(byteBuffer);
+                byteBuffer.clear();
+                sk.interestOps(SelectionKey.OP_READ);
+                state = RECEIVING;
+            } else if (state == RECEIVING) {
+                int length = 0;
+                while ((length = channel.read(byteBuffer)) > 0) {
+                    System.out.println(new String(byteBuffer.array(), 0, length));
+                }
+                byteBuffer.flip();
+                sk.interestOps(SelectionKey.OP_WRITE);
+                state = SENDING;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ nio/src/main/java/itcode/time/server/MultiThreadEchoServerReactor.java
@@ -0,0 +1,95 @@
+package itcode.time.server;
+
+import itcode.handler.MultiThreadEchoHandler;
+
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MultiThreadEchoServerReactor {
+    ServerSocketChannel serverSocketChannel;
+
+    AtomicInteger next = new AtomicInteger();
+
+    Selector[] selectors = new Selector[2];
+
+    SubReactor[] subReactors = null;
+
+    MultiThreadEchoServerReactor() throws Exception {
+        selectors[0] = Selector.open();
+        selectors[1] = Selector.open();
+        serverSocketChannel = ServerSocketChannel.open();
+        InetSocketAddress address = new InetSocketAddress("127.0.0.1", 8080);
+        serverSocketChannel.socket().bind(address);
+        serverSocketChannel.configureBlocking(false);
+        SelectionKey sk = serverSocketChannel.register(selectors[0], SelectionKey.OP_ACCEPT);
+        sk.attach(new AcceptorHandler());
+        SubReactor subReactor1 = new SubReactor(selectors[0]);
+        SubReactor subReactor2 = new SubReactor(selectors[1]);
+        subReactors = new SubReactor[]{subReactor1, subReactor2};
+    }
+
+    private void startService() {
+        new Thread(subReactors[0]).start();
+        new Thread(subReactors[1]).start();
+    }
+
+
+    class SubReactor implements Runnable {
+        final Selector selector;
+
+        public SubReactor(Selector selector) {
+            this.selector = selector;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (!Thread.interrupted()) {
+                    selector.select();
+                    Set<SelectionKey> keys = selector.selectedKeys();
+                    Iterator<SelectionKey> iterator = keys.iterator();
+                    while (iterator.hasNext()) {
+                        SelectionKey sk = iterator.next();
+                        dispatch(sk);
+                    }
+                    keys.clear();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        void dispatch(SelectionKey sk) {
+            Runnable attachment = (Runnable) sk.attachment();
+            if (attachment != null) {
+                attachment.run();
+            }
+        }
+    }
+
+
+    class AcceptorHandler implements Runnable {
+        @Override
+        public void run() {
+            try {
+                SocketChannel accept = serverSocketChannel.accept();
+                if (accept != null) {
+                    new MultiThreadEchoHandler(selectors[1], accept);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        MultiThreadEchoServerReactor server = new MultiThreadEchoServerReactor();
+        server.startService();
+    }
+}
